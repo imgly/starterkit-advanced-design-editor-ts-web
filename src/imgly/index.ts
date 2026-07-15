926cea2715861ca02626dccb5452b4a9f10427c7
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-07-15 15:52:33 +0200

    Update starterkit for v1.78.0-rc.2

--- src/imgly/index.ts
+++ src/imgly/index.ts
@@ -4,7 +4,7 @@
  * This module provides the main entry point for initializing the advanced editor.
  * Import and call `initAdvancedEditor()` to configure a CE.SDK instance for advanced editing.
  *
- * @see https://img.ly/docs/cesdk/js/getting-started/
+ * @see https://img.ly/docs/cesdk/js/get-started/overview-e18f40/
  */
 
 import type CreativeEditorSDK from '@cesdk/cesdk-js';
